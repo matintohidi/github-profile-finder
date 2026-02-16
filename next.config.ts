7fa644cf8ea6cb222b998f550859edbf209945bc
matintohidi <matintovhidi@gmail.com>
2026-02-16 22:59:26 +0330

    fix: remove output configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   reactCompiler: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
